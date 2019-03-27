a22c34c91a5dd352316967081643ef561600fafb
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-27 09:41:10 -0600

    Add Network class tests

---
+++ test/network_test.rb
@@ -0,0 +1,41 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/network'
+require '../lib/show'
+require '../lib/character'
+
+class NetworkTest < Minitest::Test
+  def setup
+    @nbc = Network.new("NBC")
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
+  end
+
+  def test_it_exists
+    assert_instance_of Show, @nbc
+  end
+
+  def test_it_has_a_name_and_zero_shows_by_default
+    assert_equal "NBC", @nbc.name
+    assert_equal []. @nbc.shows
+  end
+
+  def test_it_can_add_shows_to_its_library
+    @nbc.add_show(@knight_rider)
+    assert_equal [@knight_rider], @nbc.shows
+
+    @nbc.add_show(@parks_and_rec)
+    assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
+  end
+
+  def test_it_can_return_the_highest_paid_actor
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+
+    assert_equal "Amy Poehler", @nbc.highest_paid_actor
+  end
+end
