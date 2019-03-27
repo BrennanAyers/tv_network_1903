d3c4630ce87955dd75b8701a429055c8c9d5d595
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-27 09:10:06 -0600

    Add Character class test

---
+++ test/character_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/character'
+
+class CharacterTest < Minitest::Test
+  def setup
+    @character = Character.new("KITT", "William Daniels", 1_000_000)
+  end
+
+  def test_it_exists
+    assert_instance_of Character, @character
+  end
+
+  def test_it_has_argument_attributes
+    assert_equal "KITT", @character.name
+    assert_equal "William Daniels", @character.actor
+    assert_equal 1_000_000, @character.salary
+  end
+end
